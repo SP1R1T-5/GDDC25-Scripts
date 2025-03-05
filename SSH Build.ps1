d7047e4294ac63048aceb8c2c5ccd2fd02ff11a3
Jonathan Edwards <76668213+SP1R1T-5@users.noreply.github.com>
2025-03-05 18:01:29 -0500

    Update SSH Build.ps1

--- SSH Build.ps1
+++ SSH Build.ps1
@@ -1,20 +1,19 @@
 #Installing SSH Package
-echo"Downloading SSH..."
+write-output "Downloading SSH..."
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 #Starting SSH and enabling automatic startup
-echo"Starting SSH"
+write-output "Starting SSH"
 Start-Service sshd
 Set-Service -Name sshd -StartupType Automatic
 
 #Setting Firewall for SSH Connection
-echo"Creating Firewall Rule"
+write-output "Creating Firewall Rule"
 netsh advfirewall firewall add rule name="SSHD" dir=in action=allow protocol=TCP localport=22
 
 #Showing SSH Running
 Get-Service sshd
-netstat -an | Findstr 22
 
-echo"SSH setup complete, don't break it again >:(" 
+write-output "SSH setup complete, don't break it again >:(" 
 pause
 #Jon Fortnite
