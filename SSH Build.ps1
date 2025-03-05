16c370f0db29b005fd0a25ca8830f308b0584e28
Jonathan Edwards <76668213+SP1R1T-5@users.noreply.github.com>
2025-03-05 17:41:09 -0500

    Update SSH Build.ps1

--- SSH Build.ps1
+++ SSH Build.ps1
@@ -1,10 +1,9 @@
-Add-WindowsFeature -Name OpenSSH-Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 Start-Service sshd
 Set-Service -Name sshd -StartupType Automatic
 
-netsh advfirewall name="SSHD" dir=in protocol=TCP action=allow 
+netsh advfirewall fiewall rule add name="SSHD" dir=in protocol=TCP action=allow localport=22
 
 Get-Service sshd
 netstat -an | Findstr 22
