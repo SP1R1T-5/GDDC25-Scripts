9517d04ef7cd2309bef6b2b539ada9f658cd3110
Jonathan Edwards <76668213+SP1R1T-5@users.noreply.github.com>
2025-03-04 20:59:36 -0500

    Update Windows Domain Setup.ps1

--- Windows Domain Setup.ps1
+++ Windows Domain Setup.ps1
@@ -1,11 +1,11 @@
 #Enter IPs of the Domain Controller(s) here :)
-$IP1 = "x.x.x.x"
+$IP1 = Read-Host "Enter DC Address" 
 #$IP2 = "x.x.x.x"
 
 #Enter Credentials and Domain Name
-$Domain = "domain.local"
+$Domain = Read-Host "Enter Domain (ie. domain.local)"
 $User = "$Domain\Administrator"
-$Pass = Read-Host "Enter Password :)" -AsSecureString
+$Pass = Read-Host "Enter Password" -AsSecureString
 
 #Who wants 2 variables when you can have 1 
 $Credential = New-Object System.Management.Automation.PSCredential ($User, $Pass)
@@ -13,11 +13,14 @@ $Credential = New-Object System.Management.Automation.PSCredential ($User, $Pass
 #Setting the Preferred DNS of the Server
 echo "Pinging the DC . . . "
 ping $IP1
+echo "Setting the Prefered DNS Address"
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $IP1
 
 #Joining the Domain
+echo "Joining Domain!"
 Add-Computer -DomainName $Domain -Credential $Credential 
 
 echo "Domain is all set!"
+#Restart-Computer
+Pause
 #Jon Fortnite
-Restart-Computer
